76d49ec4e9ab7612352c03f496c613295bf9bc0f
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2024-12-28 08:58:42 +0300

    Timer Ver: 1.1

--- libs/timer.py
+++ libs/timer.py
@@ -1,28 +1,29 @@
 import sys
 sys.path.append(".")
-from libs import database
+from libs import db
 import time
 import threading
+db_path = "storage/db.db"
 
 def banned(time_ : int, IP: str):
     print(f"Starting timed ban with args time: {time_}, IP: {IP}")
     time.sleep(time_)
-    database.execute_SQL("database.db", "UPDATE Data SET time_banned = ? WHERE IP = ?", ("NULL", IP,))
-    database.execute_SQL("database.db", "UPDATE Data SET banned = ? WHERE IP = ?", ("False", IP,))
+    db.execute_SQL(db_path, "UPDATE Data SET time_banned = ? WHERE IP = ?", ("NULL", IP,))
+    db.execute_SQL(db_path, "UPDATE Data SET banned = ? WHERE IP = ?", ("False", IP,))
     print(f"Timer ban finished for IP: {IP}")
 
 
 def logined(time_ : int, IP: str):
     print(f"Starting timed login with args time: {time_}, IP: {IP}")
     time.sleep(time_)
-    database.execute_SQL("database.db", "UPDATE Data SET time_logined = ? WHERE IP = ?", ("NULL", IP,))
-    database.execute_SQL("database.db", "UPDATE Data SET logined = ? WHERE IP = ?", ("False", IP,))
+    db.execute_SQL(db_path, "UPDATE Data SET time_logined = ? WHERE IP = ?", ("NULL", IP,))
+    db.execute_SQL(db_path, "UPDATE Data SET logined = ? WHERE IP = ?", ("False", IP,))
     print(f"Timer login finished for IP: {IP}")
 
 def cookie(IP : str, time_ : int):
     print(f"Starting timed cookie with args time: {time_}, IP: {IP}")
     time.sleep(time_)
-    database.execute_SQL("database.db", "DELETE FROM Cookie WHERE own_ip = ?", (IP,))
+    db.execute_SQL(db_path, "DELETE FROM Cookie WHERE own_ip = ?", (IP,))
     print(f"Timer cookie finished for IP: {IP}")
 
 def start_timer(choice : str, IP : str, time : int):
